d8340f3fd54fd50c187397ef1ed6ea07ce2905cc
soy777 <rachmatagustio@gmail.com>
2025-09-07 02:26:41 +0700

    Create custom-post-types.php

---
+++ custom-post-types.php
@@ -0,0 +1,7 @@
+<?php
+// Custom post types registration
+function register_custom_types(){
+    // Example: register_post_type('example', [...]);
+}
+add_action('init', 'register_custom_types');
+?>
